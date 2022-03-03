26c0186cb343ef0f95be9259bdfc4785beb92852
narayana1712 <97426214+narayana1712@users.noreply.github.com>
2022-03-03 10:24:36 +0530

    #include<stdio.h> int main() { 	char str[100]; 	scanf("%s",&str); 	int i; 	for(i=0;str[i] !=NULL;i++) 	{ 		if(str[i]>='A' && str[i]<='Z') 		{ 			str[i]=str[i]+32; 		} 	} 	printf("%s ",str); }

---
+++ To_Lower_Case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	scanf("%s",&str);
+	int i;
+	for(i=0;str[i] !=NULL;i++)
+	{
+		if(str[i]>='A' && str[i]<='Z')
+		{
+			str[i]=str[i]+32;
+		}
+	}
+	printf("%s
+",str);
+}
