aaedd29232bfc1151ab20a72521502013e7bdb15
narayana1712 <97426214+narayana1712@users.noreply.github.com>
2022-05-28 15:54:30 +0530

    #include<stdio.h> int main() {     int n,i,j,alphabet=65;;     scanf("%d",&n);     for(i=n-1;i>=0;i--)     {         for(j=0;j<=i;j++)         {             printf("%c ",(alphabet+i));         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,9 +3,9 @@ int main()
 {
     int n,i,j,alphabet=65;;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(i=n-1;i>=0;i--)
     {
-        for(j=0;j<n;j++)
+        for(j=0;j<=i;j++)
         {
             printf("%c ",(alphabet+i));
         }
