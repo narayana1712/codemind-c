e264e370e7a589a3c51e7d373eae198c9406de60
narayana1712 <97426214+narayana1712@users.noreply.github.com>
2022-06-27 21:18:51 +0530

    #include<stdio.h> int main() {     int n,c=0,i,j;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             c++;         }         if(c==1)         {             printf("%d",arr[i]);             break;         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            c++;
+        }
+        if(c==1)
+        {
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+}
